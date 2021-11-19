07362ecf8d077b5dfb65ddfaf4310b9ab5b43b1d
Vasil Telyachuk <Vasil.Telyachuk@gmail.com>
2021-11-19 08:10:31 +0200

    task1

---
+++ task-01.js
@@ -0,0 +1,18 @@
+const friends = ['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong'];
+const numburs = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
+
+const logItems = function(array) {
+    for (let i=0; i<=friends.length; i+=1) {
+        console.log(friends);
+           }
+// for (const item of array) {
+//     console.log(`${} -`,item);
+//    }
+  };
+  
+  /*
+   * Вызовы функции для проверки работоспособности твоей реализации.
+   */
+  logItems(friends);
+  
+  logItems(numburs);
